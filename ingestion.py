080cafa3b3b7c745edcf1e6438ca27a16b599921
raja-jain-skydio <raja.jain@skydio.com>
2024-03-04 10:44:08 -0800

    ollama embeddings implementation

--- ingestion.py
+++ ingestion.py
@@ -5,7 +5,7 @@ load_dotenv()
 import os
 
 from langchain_community.document_loaders import ReadTheDocsLoader
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Pinecone as PineconeLangChain
 from pinecone import Pinecone
@@ -15,25 +15,24 @@ pc = Pinecone(
     api_key=os.environ.get("PINECONE_API_KEY"),
 )
 
-INDEX_NAME = "langchain-doc-index"
-
+INDEX_NAME = "documentation-helper"
+MODEL = "llama2"
+DOCS_PATH = "langchain-docs/"
 
 def ingest_docs():
-    loader = ReadTheDocsLoader(
-        "langchain-docs/api.python.langchain.com/en/latest/chains"
-    )
+    loader = ReadTheDocsLoader(DOCS_PATH)
 
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
     documents = text_splitter.split_documents(raw_documents)
-    for doc in documents:
+    for doc in documents[0:25]:
         new_url = doc.metadata["source"]
         new_url = new_url.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    embeddings = OllamaEmbeddings(model=MODEL, show_progress=True)
     print(f"Going to add {len(documents)} to Pinecone")
     PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
     print("****Loading to vectorstore done ***")
