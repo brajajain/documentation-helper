ee54d192ca026127397b2755c0716fcb1a3c3f86
raja-jain-skydio <raja.jain@skydio.com>
2024-03-04 11:48:48 -0800

    chat with memory implementation

--- backend/core.py
+++ backend/core.py
@@ -7,7 +7,7 @@ from langchain_community.embeddings import OllamaEmbeddings
 
 import os
 from typing import Any
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
 from pinecone import Pinecone
 
@@ -19,19 +19,21 @@ MODEL = "llama2"
 DOCS_PATH = "langchain-docs/"
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history) -> Any:
     embeddings = OllamaEmbeddings(model=MODEL, show_progress=True)
     docsearch = PineconeLangChain.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOllama(model=MODEL,verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
+    
+    qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
         chain_type="stuff",
         retriever=docsearch.as_retriever(),
         return_source_documents=True,
     )
-    return qa({"query": query})
+    
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
